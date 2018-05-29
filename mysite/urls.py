3b3ba5f14f660bddca6feda10569fcb718d2a5af
unknown <joseph.dateson@gmail.com>
2018-05-29 21:25:41 +0300

    Delete mysite

--- mysite/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-import math
-urlpatterns = [
-    url(r'^', include('polls.urls')),
-	url(r'^result/', include('polls.urls')),
-]
